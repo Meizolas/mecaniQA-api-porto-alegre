e66b9da13f8c218d4bb2dbcb565ca8c769539367
Kevy06 <kevilynbitencourt14@gmail.com>
2026-08-29 12:18:32 -0300

    feat: adiciona endpoints REST de pecas

--- src/main/java/br/com/mecaniqa/controller/PecaController.java
+++ src/main/java/br/com/mecaniqa/controller/PecaController.java
@@ -1,8 +1,19 @@
 package br.com.mecaniqa.controller;
 
+import br.com.mecaniqa.model.Peca;
 import br.com.mecaniqa.repository.PecaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pecas")
@@ -12,5 +23,41 @@ public class PecaController {
 
     public PecaController() {
         this.repository = PecaRepository.getInstance();
+    }
+
+    @PostMapping
+    public ResponseEntity<Peca> cadastrar(@RequestBody Peca peca) {
+        Peca pecaSalva = repository.salvar(peca);
+        return ResponseEntity.status(HttpStatus.CREATED).body(pecaSalva);
+    }
+    @GetMapping
+    public ResponseEntity<List<Peca>> listar() {
+        return ResponseEntity.ok(repository.listar());
+    }
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Peca> buscarPorCodigo(@PathVariable Long codigo) {
+        return repository.buscarPorCodigo(codigo)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+    @PutMapping("/{codigo}")
+    public ResponseEntity<Peca> atualizar(
+            @PathVariable Long codigo,
+            @RequestBody Peca novosDados) {
+
+        return repository.atualizar(codigo, novosDados)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{codigo}")
+    public ResponseEntity<Void> excluir(@PathVariable Long codigo) {
+        boolean excluiu = repository.excluir(codigo);
+
+        if (excluiu) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
