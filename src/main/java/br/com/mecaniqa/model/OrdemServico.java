226a059b1c5bcce604c967a92be509ef8233b170
Helio <HelioJR780@gmail.com>
2026-09-11 21:34:40 -0300

    docs: ordemservico criado, falta builder

---
+++ src/main/java/br/com/mecaniqa/model/OrdemServico.java
@@ -0,0 +1,67 @@
+package br.com.mecaniqa.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class OrdemServico {
+
+    private Long id;
+    private String descricaoProblema;
+    private LocalDate dataAbertura;
+    private StatusOrdemServico status;
+    private double valor;
+
+    public OrdemServico(
+            Long id,
+            String descricaoProblema,
+            LocalDate dataAbertura,
+            StatusOrdemServico status,
+            double valor) {
+
+        this.id = id;
+        this.descricaoProblema = descricaoProblema;
+        this.dataAbertura = dataAbertura;
+        this.status = status;
+        this.valor = valor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricaoProblema() {
+        return descricaoProblema;
+    }
+
+    public void setDescricaoProblema(String descricaoProblema) {
+        this.descricaoProblema = descricaoProblema;
+    }
+
+    public LocalDate getDataAbertura() {
+        return dataAbertura;
+    }
+
+    public void setDataAbertura(LocalDate dataAbertura) {
+        this.dataAbertura = dataAbertura;
+    }
+
+    public StatusOrdemServico getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusOrdemServico status) {
+        this.status = status;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
